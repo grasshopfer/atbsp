061b31034374a020852f1668d0160a0ef530c470
grasshopfer <k.gainey@protonmail.com>
2019-05-28 00:37:51 -0600

    resume lucky.py, parse json

--- chapter11/google/lucky.py
+++ chapter11/google/lucky.py
@@ -1,13 +1,27 @@
 #! python3
 
-import webbrowser, sys, requests
+import webbrowser, sys, requests, bs4
 
 # TODO get search keywords from command line args
-args = sys.argv
-keyword = 'poop'
+
 # TODO retrieve and parse search results page
-searchURL = 'https://duckduckgo.com/?q=%s&ia=web' % (keyword)
-print(searchURL)
+#print('DuckDuckGo...ing...')       # this returns 418 :(
+#res = requests.get('http://duckduckgo.com/?q=' + '+'.join(keywords))
+
+print('Googling...')
+res = requests.get('http://google.com/search?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
+
+# retrieve top search results
+soup = bs4.BeautifulSoup(res.text, "html5lib")
+print(soup.text)
+
+linkElems = soup.select('.r a')
 # TODO open a browser tab for the top three results
 
+numOpen = min(5, len(linkElems))
+print(len(linkElems))
+for i in range(numOpen):
+    print(numOpen)
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
 
